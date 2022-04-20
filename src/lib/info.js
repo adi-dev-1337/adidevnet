f232b64f8c9ea6006ad0a243507028bb7a615432
adi-dev-1337 <adi.dev.1337@gmail.com>
2022-04-20 16:56:08 +0100

    remove name

--- src/lib/info.js
+++ src/lib/info.js
@@ -5,7 +5,7 @@
 /**
  * Used for the header of the page and in SEO
  */
-export const name = 'Adi' // replace with your name
+export const name = '' // replace with your name
 
 /**
  * Used for SEO in /posts/[slug]
